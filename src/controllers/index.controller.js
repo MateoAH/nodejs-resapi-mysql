638ec794e42be4102d3f7dce7bda78b314b693c4
Mateo Agudelo Holguin <mateo.agudelo@ias.com.co>
2024-06-29 14:07:22 -0500

    error ping

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,12 @@
 import {pool} from '../db.js';
 
 export const ping = async (req, res) => {
-    let [result] = await pool.query('SELECT "pong" AS result');
-    res.json(result);
+    try {
+        
+        let [result] = await pool.query('SELECT "pong" AS result');
+        res.json(result);
+    } catch (error) {
+        res.status(500).send({message: "hubo un error"})
+    }
 
 }
